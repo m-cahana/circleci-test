d8b6bea38e7eadd2a05e45529685e96d6d8f1b51
Michael Cahana <cahanamichael@gmail.com>
2019-01-29 09:55:19 -0600

    correcting test_data.xlsx path

--- test/second_test.R
+++ test/second_test.R
@@ -8,7 +8,7 @@ library(readxl)
 # actual_size <- nrow(random_half_split(iris))
 # expect_equal(actual_size, 75)
 
-test_data <- read_excel('test_data.xlsx')
+test_data <- read_excel('test/test_data.xlsx')
 max <- test_data %>% filter(object=='max') %>% pull(value)
 expect_lte(max, 9)
 min <- test_data %>% filter(object=='min') %>% pull(value)
